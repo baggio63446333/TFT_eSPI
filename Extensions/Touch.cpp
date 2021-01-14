f8c191e5721f19e0d8e5e1ba28c043dd31073513
baggio63446333 <baggio63446333@users.noreply.github.com>
2021-01-14 22:34:16 +0900

    Change implementation to get raw data of touchscreen

--- Extensions/Touch.cpp
+++ Extensions/Touch.cpp
@@ -52,7 +52,50 @@ void TFT_eSPI::spi_end_touch()   {  end_touch_read_write();}
 ** Function name:           getTouchRaw
 ** Description:             read raw touch position.  Always returns true.
 ***************************************************************************************/
+#define Z_THRESHOLD 350 // Touch pressure threshold for validating touches
+#ifdef ARDUINO_ARCH_SPRESENSE
+static int16_t besttwoavg( int16_t x , int16_t y , int16_t z ) {
+  int16_t da, db, dc;
+  int16_t reta = 0;
+  if ( x > y ) da = x - y; else da = y - x;
+  if ( x > z ) db = x - z; else db = z - x;
+  if ( z > y ) dc = z - y; else dc = y - z;
+
+  if ( da <= db && da <= dc ) reta = (x + y) >> 1;
+  else if ( db <= da && db <= dc ) reta = (x + z) >> 1;
+  else reta = (y + z) >> 1;   //    else if ( dc <= da && dc <= db ) reta = (x + y) >> 1;
+
+  return (reta);
+}
+#endif
+
 uint8_t TFT_eSPI::getTouchRaw(uint16_t *x, uint16_t *y){
+#ifdef ARDUINO_ARCH_SPRESENSE
+  begin_touch_read_write();
+
+	int16_t data[6];
+	int z;
+  spi.transfer(0xB1 /* Z1 */);
+  int16_t z1 = spi.transfer16(0xC1 /* Z2 */) >> 3;
+  z = z1 + 4095;
+  int16_t z2 = spi.transfer16(0x91 /* X */) >> 3;
+  z -= z2;
+
+  if (z >= Z_THRESHOLD) {
+    spi.transfer16(0x91 /* X */);  // dummy X measure, 1st is always noisy
+    data[0] = spi.transfer16(0xD1 /* Y */) >> 3;
+    data[1] = spi.transfer16(0x91 /* X */) >> 3; // make 3 x-y measurements
+    data[2] = spi.transfer16(0xD1 /* Y */) >> 3;
+    data[3] = spi.transfer16(0x91 /* X */) >> 3;
+  }
+  else data[0] = data[1] = data[2] = data[3] = 0;	// Compiler warns these values may be used unset on early exit.
+  data[4] = spi.transfer16(0xD0 /* Y */) >> 3;	// Last Y touch power down
+  data[5] = spi.transfer16(0) >> 3;
+
+	*y = besttwoavg( data[0], data[2], data[4] );
+	*x = besttwoavg( data[1], data[3], data[5] );
+
+#else
   uint16_t tmp;
 
   begin_touch_read_write();
@@ -85,7 +128,7 @@ uint8_t TFT_eSPI::getTouchRaw(uint16_t *x, uint16_t *y){
   tmp |= 0x1f & (spi.transfer(0)>>3);    // Read last 8 bits
 
   *y = tmp;
-
+#endif
   end_touch_read_write();
 
   return true;
@@ -159,7 +202,7 @@ uint8_t TFT_eSPI::validTouch(uint16_t *x, uint16_t *y, uint16_t threshold){
 ** Function name:           getTouch
 ** Description:             read callibrated position. Return false if not pressed. 
 ***************************************************************************************/
-#define Z_THRESHOLD 350 // Touch pressure threshold for validating touches
+
 uint8_t TFT_eSPI::getTouch(uint16_t *x, uint16_t *y, uint16_t threshold){
   uint16_t x_tmp, y_tmp;
   
